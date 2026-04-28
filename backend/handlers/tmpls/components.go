5eec99cf77b22ca38b57f2a4b7c98dc65d4352ac
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2026-04-28 16:59:59 +0000

    :sparkles: refactor data-ref

--- backend/handlers/tmpls/components.go
+++ backend/handlers/tmpls/components.go
@@ -2,10 +2,12 @@ package tmpls
 
 import (
 	"html/template"
+
+	"github.com/Masterminds/sprig/v3"
 )
 
 var componentTmpls *template.Template
 
 func init() {
-	componentTmpls, _ = template.ParseGlob("resources/templates/components/**/*.html")
+	componentTmpls, _ = template.New("components").Funcs(sprig.FuncMap()).ParseGlob("resources/templates/components/**/*.html")
 }
